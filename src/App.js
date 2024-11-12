8920527c9f7b8778dd5b74d703b31c61ea0a48f6
FernandoROL <felhopez@gmail.com>
2024-11-12 11:52:54 -0300

    first class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
 
-const Welcome = () => <h2>Welcome!</h2>;
-
-export default function App(){
-  return(
+const Team = (props) => {
+  return (
     <div>
-      <h1>React course!</h1>
-      <Welcome/>
+      <Sobre name={props.name} role={props.role} age={props.age}></Sobre>
+      <Social fb={props.fb} in={props.in} ig={props.ig}/>
+    </div>
+  );
+}
+
+const Sobre = (props) => {
+  return (
+    <div>
+      <h2>Hello I'm {props.name}</h2>
+      <h3>Role: {props.role}</h3>
+      <h3>Age: {props.age}</h3>
+    </div>
+  );
+}
+
+const Social = (props) => {
+  return (
+    <div>
+      <a href={props.fb}>Facebook</a>
+      <br/>
+      <a href={props.in}>Linkedin</a>
+      <br/>
+      <a href={props.ig}>Instagram</a>
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <div>
+      <h1>Get to know out team:</h1>
+      <Team name="Fernando" role="Web Developer" age="20" fb='https://facebook.com' in='https://linkedin.com' ig='https://instagram.com'></Team>
+      <Team name="Isabella" role="CEO" age="35" fb='https://facebook.com' in='https://linkedin.com' ig='https://instagram.com'></Team>
     </div>
   )
 }
