c521388e59430de3f97597a957016cbc9d227008
FernandoROL <felhopez@gmail.com>
2024-11-12 15:28:12 -0300

    fith class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from "react";
-import Member from "./components/member";
 
-class App extends Component{
-  render(){
-    return(
-      <div> 
-        <Member></Member>
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: false,
+      name: props.name
+    }
+
+    this.log = this.log.bind(this);
+  }
+
+  log(){
+    this.state.status ? 
+    this.setState({status: false, name: ''}) :
+    this.setState({status: true, name: prompt('Name:')})
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.status ?
+          <div>
+            <h1>Welcome, {this.state.name}!</h1>
+            <button onClick={this.log}>Logout</button>
+          </div>
+          :
+          <div>
+            <h2>Hi Guest! Please, log in.</h2>
+            <button onClick={this.log}>Login</button>
+          </div>
+        }
       </div>
     );
   }
