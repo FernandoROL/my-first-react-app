b1b9e28f6a7891a5dbef8ad9aae6725f8d5852f0
FernandoROL <felhopez@gmail.com>
2024-11-12 15:55:18 -0300

    sixth class

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
 import React, { Component } from "react";
+import Feed from "./components/Feed";
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      status: false,
-      name: props.name
+      feed: [
+        {id: 1, user: 'Fernando', likes: 20, comments: 2},
+        {id: 2, user: 'Lucas', likes: 69, comments: 24},
+        {id: 3, user: 'Mateus', likes: 200, comments: 21},
+        {id: 4, user: 'Joelma', likes: 1, comments: 1},
+      ]
     }
-
-    this.log = this.log.bind(this);
-  }
-
-  log(){
-    this.state.status ? 
-    this.setState({status: false, name: ''}) :
-    this.setState({status: true, name: prompt('Name:')})
   }
 
   render() {
     return (
       <div>
-        {this.state.status ?
-          <div>
-            <h1>Welcome, {this.state.name}!</h1>
-            <button onClick={this.log}>Logout</button>
-          </div>
-          :
-          <div>
-            <h2>Hi Guest! Please, log in.</h2>
-            <button onClick={this.log}>Login</button>
-          </div>
+        {
+          this.state.feed.map((item) => {
+            return(
+              <Feed key={item.id} user={item.user}
+               likes={item.likes} comments={item.comments}></Feed>
+            );
+          })
         }
       </div>
     );
