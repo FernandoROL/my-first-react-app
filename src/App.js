6c578d8fc053dd6c3af39d686dd415e09b053822
FernandoROL <felhopez@gmail.com>
2024-11-13 10:12:26 -0300

    fortune cookie

--- src/App.js
+++ src/App.js
@@ -1,73 +1,45 @@
 import React, { Component } from "react";
+import { fortunes } from "./assets/fortunes";
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      form: {
-        name: 'teste',
-        email: '123123@example.com',
-        password: '3333',
-        gender: ''
-      },
-    }
-    this.register = this.register.bind(this);
-    this.formChange = this.formChange.bind(this);
+      phraseText: 'Click the cookie...'
+    };
+    this.phrase = fortunes;
+
+    this.biscuitFortune = this.biscuitFortune.bind(this);
   }
 
-  register(event) {
-    const { name, email, password, gender } = this.state.form;
-    name !== '' && email !== '' && password !== '' && gender != '' ?
-      alert(`All fields filled in! \nSelling your information to the chinese mafia... \n\nName: ${name} \nEmail: ${email} \nPassword: ${password} \nGender: ${gender}`) :
-      this.setState({ error: 'Looks like you missed some fieds...' });
-    event.preventDefault();
+  biscuitFortune(){
+    let state = this.state;
+    let randomNumber = Math.floor(Math.random() * this.phrase.length);
+    state.phraseText = this.phrase[randomNumber];
+    this.setState(state);
   }
-
-  formChange(e){
-    let form = this.state.form;
-    form[e.target.name] = e.target.value;
-    this.setState({form: form});
-  }
-
 
   render() {
     return (
+      <div className="main">
+        <Botao btnAction={this.biscuitFortune} name='Give me fortune'/>
+        <h3 className="phrase">{`" ${this.state.phraseText} "`}</h3>
+      </div>
+    );
+  }
+}
+
+class Botao extends Component {
+  render() {
+    return (
       <div>
-        <h1>New user</h1>
-        {this.state.error && <p id='error-alert'>{this.state.error}</p>}
-        <form onSubmit={this.register}>
-          <div>
-            <label>Name:</label>
-            <input type="text" name="name" value={this.state.form.name}
-              onChange={this.formChange}></input>
+        <div className="container">
+          <img id="biscuit" src={require("./assets/biscoito.png")} />
+          <div className="middle">
+            <button id="fortune-button" onClick={this.props.btnAction}>{this.props.name}</button>
           </div>
-          <br /><br />
-          <div>
-            <label>Email:</label>
-            <input type="email" name="email" value={this.state.form.email} 
-              onChange={this.formChange}></input>
-          </div>
-          <br /><br />
-          <div>
-            <label>Password:</label>
-            <input type="password" name="password" value={this.state.form.password} 
-              onChange={this.formChange}></input>
-          </div>
-          <br /><br />
-          <div>
-            <label>Gender:</label>
-            <select name="gender" value={this.state.form.gender} 
-              onChange={this.formChange}>
-              <option value={''}>Select Gender</option>
-              <option value={'male'}>Male</option>
-              <option value={'female'}>Female</option>
-              <option value={'testes'}>Don't specify</option>
-            </select>
-          </div>
-          <br /><br />
-          <button type="submit">Submit</button>
-        </form>
+        </div>
       </div>
     );
   }
